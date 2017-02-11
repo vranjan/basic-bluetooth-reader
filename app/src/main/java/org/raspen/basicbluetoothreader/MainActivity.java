aa34cf71e62a26b69e6711a8b61a521e1e4d0951
Cody Raspen <cody+github@raspen.org>
2017-02-11 16:14:39 -0500

    Add button to launch device selection dialog

--- app/src/main/java/org/raspen/basicbluetoothreader/MainActivity.java
+++ app/src/main/java/org/raspen/basicbluetoothreader/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -28,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button mSelectButton = (Button) findViewById(R.id.select_device_button);
+        mSelectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSelectDialog();
+            }
+        });
+    }
+
+    private void showSelectDialog() {
         BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
 
