d45f27e75a9d552167d4d29eeab3b687f85c7fbd
Cody Raspen <cody+github@raspen.org>
2017-02-11 16:00:18 -0500

    Add dialog to select paired device

--- app/src/main/java/org/raspen/basicbluetoothreader/MainActivity.java
+++ app/src/main/java/org/raspen/basicbluetoothreader/MainActivity.java
@@ -1,13 +1,65 @@
 package org.raspen.basicbluetoothreader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    List<String> deviceNames = new ArrayList<>();
+    final List<String> devices = new ArrayList<>();
+
+    String selectedDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                deviceNames.add(device.getName() + "\n" + device.getAddress());
+                devices.add(device.getAddress());
+            }
+        } else {
+            showToast("No paired devices found.");
+        }
+
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_singlechoice,
+                deviceNames.toArray(new String[deviceNames.size()]));
+
+        alertDialog.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                int position = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+                selectedDevice = devices.get(position);
+                Log.d(TAG, "Selected device: " + selectedDevice);
+            }
+        });
+
+        alertDialog.setTitle("Select a device");
+        alertDialog.show();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
